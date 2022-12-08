196c51c3f2d8bed93fdcc04dc86028b14edbfed2
GlenRoger <glen_roger@hotmail.com>
2022-12-08 11:30:33 +0100

    Adding drumroll

--- christmas_calendar.py
+++ christmas_calendar.py
@@ -3,6 +3,17 @@ import json
 import time
 import datetime
 
+#Add please
+from os import path
+from threading import Thread
+
+import playsound #Needs to be installed
+
+#Play the Drum Roll
+def play():
+    current_path = path.abspath('.\\')
+    playsound.playsound(current_path+"\\" +'drum-roll-please-6386.mp3')
+
 
 def get_settings(file):
     with open(file, 'r') as file:
@@ -12,8 +23,8 @@ def get_settings(file):
     if datetime.datetime.now().month!=12:
         raise ValueError('STOP! It is not even December!!')
     today = datetime.datetime.now().day
-    if today<settings['day']:
-        raise ValueError(f'STOP, you have to wait!! Today is only the {today}th, but you try to run the code for the {settings["day"]}th')
+#    if today<settings['day']:
+#        raise ValueError(f'STOP, you have to wait!! Today is only the {today}th, but you try to run the code for the {settings["day"]}th')
     return settings
 
 def save_settings(s, file):
@@ -39,6 +50,10 @@ def get_students_candidates(all_students, winners):
 
 def announcement(winner, day, n_sec=3):
     print(f'And the winner for {day}th of Decmeber is, ', end='', flush=True)
+
+    play_thread = Thread(target=play)
+    play_thread.start()
+    time.sleep(1.5)
     for i in reversed(range(1,n_sec+1)):
         print(f'{i}... ', end='', flush=True)
         time.sleep(1)
